51bf9060f75496c053b493bce2101694943da818
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-03-03 00:47:38 +0000

    implement bad characters generation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 extern crate argparse;
 fn main() {
-  println!("Hello world!");
+  let chars_to_skip: Vec<u8> = Vec::new();
+  let mut badchars: String = String::new();
+
+  for x in 0..=255{
+    if chars_to_skip.contains(&x){continue;}
+    badchars = format!("{}\\x{x:02x}",badchars);
+  }
+
+  println!("{}", badchars);
 }
